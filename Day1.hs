755dc98797fd8456309daf6683af971e447dc77c
rrruko <rukokarasu@gmail.com>
2021-11-30 23:30:44 -0800

    day1

---
+++ Day1.hs
@@ -0,0 +1,32 @@
+import Data.Foldable (foldl')
+
+part1 :: [Int] -> Int
+part1 (x:xs) =
+  let
+    (_, count) = foldl' (flip accumulate) (x, 0) xs
+  in
+    count
+  where
+  accumulate x (prev, count)
+    | x > prev = (x, count + 1)
+    | otherwise = (x, count)
+
+window :: Int -> [a] -> [[a]]
+window n xs
+  | length w < n = []
+  | otherwise = w : window n (tail xs)
+  where
+  (w, rest) = splitAt n xs
+
+sumWindows :: Num a => Int -> [a] -> [a]
+sumWindows n xs = map sum (window n xs)
+
+part2 :: [Int] -> Int
+part2 = part1 . sumWindows 3
+
+main :: IO ()
+main = do
+  input <- readFile "day1"
+  let nums = map read (lines input)
+  print (part1 nums)
+  print (part2 nums)
